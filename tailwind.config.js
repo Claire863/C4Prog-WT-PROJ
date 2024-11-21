be51e3fe3d02c41784137e6ba43130e121049675
Claire Rives <20104019@tafe.wa.edu.au>
2024-11-21 14:04:58 +0800

    feat: add images and tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        'cambridge-blue': '#77ACA2',
+        'ash-gray': '#9DBEBB',
+        'charcoal': '#031926',
+        'alice-blue': '#F4FBFC',
+        'teal':'#468189',
+      },
+    },
+  },
+  plugins: [],
+}
+
